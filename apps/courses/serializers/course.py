882c99dfe22accb747a3b433079197e528c74c1e
satel31 <blokhnina.tatiana@yandex.ru>
2023-07-18 01:03:11 +0300

    Added count of lessons for serializer

--- apps/courses/serializers/course.py
+++ apps/courses/serializers/course.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
-from apps.courses.models import Course
+from apps.courses.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
+    def get_lesson_count(self, course):
+        return Lesson.objects.filter(course=course.pk).count()
+
     class Meta:
         model = Course
         fields = '__all__'
