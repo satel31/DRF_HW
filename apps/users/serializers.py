3e93a3a7be7121bfbf9d0cce07cd47631b1ef444
satel31 <blokhnina.tatiana@yandex.ru>
2023-07-19 23:23:54 +0300

    Added method to hash password

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 from apps.courses.models import Payment
 from apps.users.models import User
 
+from django.contrib.auth.hashers import make_password
+
 
 class UserSerializer(serializers.ModelSerializer):
     payments = serializers.SerializerMethodField()
@@ -10,6 +12,15 @@ class UserSerializer(serializers.ModelSerializer):
     def get_payments(self, user):
         return Payment.objects.filter(user=user.pk).values()
 
+    def validate_password(self, value: str) -> str:
+        """
+        Hash value passed by user.
+
+        :param value: password of a user
+        :return: a hashed version of the password
+        """
+        return make_password(value)
+
     class Meta:
         model = User
-        fields = ('email', 'city', 'avatar', 'phone', 'payments',)
+        fields = ('email', 'city', 'avatar', 'phone', 'payments', 'password')
