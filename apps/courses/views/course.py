67809f4da4d96b52efc63f40d6815683c7d7e2dd
satel31 <blokhnina.tatiana@yandex.ru>
2023-07-19 11:51:28 +0300

    Deleted serializers

--- apps/courses/views/course.py
+++ apps/courses/views/course.py
@@ -2,17 +2,8 @@ from rest_framework.viewsets import ModelViewSet
 
 from apps.courses.models import Course
 from apps.courses.serializers.course import CourseSerializer
-#, CourseListSerializer, CourseDetailSerializer
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    #default_serializer = CourseSerializer
-    #serializers = {
-        #'list': CourseListSerializer,
-        #'retrieve': CourseDetailSerializer,
-    #}
-
-    #def get_serializer_class(self):
-        #return self.serializers.get(self.action, self.default_serializer)
