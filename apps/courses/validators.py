f518e7058d45a9bffac82d80e552f0c5a9f306d6
satel31 <blokhnina.tatiana@yandex.ru>
2023-07-25 18:53:53 +0300

    Added validator for video link

---
+++ apps/courses/validators.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+
+class VideoLinkValidator:
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        tmpvalue = value.get(self.field)
+        if tmpvalue[12:23] != 'youtube.com':
+            raise serializers.ValidationError('You can add only youtube videos')
