9ff9cce32cb6d4c84001ebf35467339c18916440
satel31 <blokhnina.tatiana@yandex.ru>
2023-07-27 02:35:01 +0300

    Added field payment_link

--- apps/courses/serializers/subscription.py
+++ apps/courses/serializers/subscription.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from apps.courses.models import Subscription
+from apps.courses.models import Subscription, Course
+from apps.courses.services import get_pay_link
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    payment_link = serializers.SerializerMethodField()
+
+    def get_payment_link(self, instance):
+        obj = Course.objects.get(pk=instance.course.pk)
+        return get_pay_link(obj)
+
     class Meta:
         model = Subscription
         fields = '__all__'
