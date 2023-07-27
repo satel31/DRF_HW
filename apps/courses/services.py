8a51802cf29b2ead27c1c570bc2e5e3a230c8977
satel31 <blokhnina.tatiana@yandex.ru>
2023-07-27 22:46:45 +0300

    Added functions for getting status of payment and regular checking

--- apps/courses/services.py
+++ apps/courses/services.py
@@ -1,5 +1,9 @@
 import requests
+import schedule
 from django.conf import settings
+
+from apps.courses.models import Payment
+
 
 def create_product(product):
     url = 'https://api.stripe.com/v1/products'
@@ -12,6 +16,7 @@ def create_product(product):
     }
     response = requests.post(url, headers=headers, params=params)
     return response.json()['id']
+
 
 def create_price(product, price):
     url = 'https://api.stripe.com/v1/prices'
@@ -41,7 +46,43 @@ def get_pay_link(obj):
         'success_url': 'https://example.com/success',
     }
     response = requests.post(url, headers=headers, params=params)
-    response_get = requests.get(f'{url}/{response.json()["id"]}', headers=headers)
-    print(response_get.json()['payment_status'])
-    return response.json()['url']
+    return response.json()
 
+
+def get_payment_status(payment_id):
+    url = f'https://api.stripe.com/v1/checkout/sessions/{payment_id}'
+    headers = {
+        'Authorization': f'Bearer {settings.STRIPE_API_KEY}',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        return response.json()['status']
+    except KeyError:
+        print(f'There is no payment with id {payment_id}')
+        return None
+
+
+def create_payment_object(obj, data):
+    pay_data = {
+        'user': obj.user,
+        'course': obj.course,
+        'sum': data['amount_total'],
+        'method': 'card',
+        'payment_id': data['id'],
+        'payment_status': get_payment_status(data['id'])
+    }
+
+    Payment.objects.create(**pay_data)
+
+def check_payment_status():
+    payments = Payment.objects.all()
+    for payment in payments:
+        status = get_payment_status(payment.payment_id)
+        payment.payment_status = status
+        payment.save()
+        print(status)
+
+def run_schedule():
+    """Run a schedule"""
+    schedule.every(12).hours.do(check_payment_status)
