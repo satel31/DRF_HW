3c2d3c80a8cb5b52eec674d7d602f045222f6447
satel31 <blokhnina.tatiana@yandex.ru>
2023-08-27 18:47:55 +0300

    Changed db settings

--- config/settings.py
+++ config/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'mysecretpassword',
-        'HOST': 'db'
+        #'PASSWORD': 'mysecretpassword',
+        #'HOST': 'db'
     }
 }
 
